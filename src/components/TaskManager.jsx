e9127aec3ff2aa3447f1432eb4cb1ebbcde73f23
Sonkad <kazykeldidosanov@gmail.com>
2025-03-04 16:56:03 +0600

    added saved Items

--- src/components/TaskManager.jsx
+++ src/components/TaskManager.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TaskForm from "./TaskForm";
 import TaskList from "./TaskList";
 import '../App.css';
@@ -7,7 +7,35 @@ import '../App.css';
 
 
 export default function TaskManager() {
+
   const [tasks, setTasks] = useState([]);
+
+
+  useEffect(() => {
+    getItemtoLocal()
+  }, [])
+
+  useEffect(() => {
+    saveItemtoLocal()
+  }, [tasks])
+
+  const saveItemtoLocal = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }
+  const getItemtoLocal = () => {
+    if (localStorage.getItem('tasks') === null) {
+      localStorage.setItem('tasks', JSON.stringify([]))
+    } else {
+      const savedTasks = JSON.parse(localStorage.getItem('tasks'));
+      if (savedTasks && savedTasks.length > 0) {
+        setTasks(savedTasks);
+      }
+    }
+  }
+
+
+
+
 
   const addTask = (text) => {
     const newTask = { id: Date.now(), text, completed: false };
